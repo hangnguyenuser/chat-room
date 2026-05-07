54f931acd009be5b6bde4f15aa9a0372056298d9
hangnguyenuser <hangnguyen.user@gmail.com>
2026-05-07 00:54:13 -0400

    port

--- server/websocket.ts
+++ server/websocket.ts
@@ -103,6 +103,8 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(3001, () => {
-	console.log("server running on port 3001");
+const PORT = Number(process.env.PORT) || 3001;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on", PORT);
 });
